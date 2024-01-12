e023188b10baed87b61f0566017ff71f591f7ce4
Roberto Garcia <146500647+robertinhus@users.noreply.github.com>
2024-01-12 00:41:51 -0600

    Delete checkInternet.sh

--- checkInternet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-salida=$(speedtest | grep Result)
-curl -X POST \
-     -H 'Content-Type: application/json' \
-     -d '{"chat_id": "-0123456789ABC", "text": "'"$salida"'", "disable_notification": true}' \
-     https://api.telegram.org/botTOKEN-NUMBER/sendMessage
